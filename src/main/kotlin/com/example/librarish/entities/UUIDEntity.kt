f811d7d0a97c289db546c7c77777f74d96ae42ce
Isobel Ibironke <isobel_ibironke@icloud.com>
2024-09-27 09:56:19 +0100

    Make UUID a val

--- src/main/kotlin/com/example/librarish/entities/UUIDEntity.kt
+++ src/main/kotlin/com/example/librarish/entities/UUIDEntity.kt
@@ -2,11 +2,13 @@ package com.example.librarish.entities
 
 import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
 import java.util.*
 
 
 @MappedSuperclass
 class UUIDEntity(
-    @Id
-    var id: UUID = UUID.randomUUID()
+    @Id @JdbcTypeCode(SqlTypes.UUID)
+    val id: UUID = UUID.randomUUID()
 )
