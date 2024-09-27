0edc1561038362c4a87be3cf4eda1d252c2bcb77
Isobel Ibironke <isobel_ibironke@icloud.com>
2024-09-27 12:00:06 +0100

    Clear login error message if user updates details

--- src/main/resources/static/assets/js/loginButtonHandler.js
+++ src/main/resources/static/assets/js/loginButtonHandler.js
@@ -2,7 +2,9 @@ const loginForm = document.getElementById("loginForm");
 const nameInput = document.getElementById("name");
 const passwordInput = document.getElementById("password");
 const loginButton = document.getElementById("loginButton");
+const errorMessage = document.getElementById("errorMessage");
 
 loginForm.addEventListener("input", () => {
     loginButton.disabled = nameInput.value === "" || passwordInput.value === "";
+    errorMessage.textContent = "";
 });
